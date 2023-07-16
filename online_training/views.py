77ae18c8c87c98d9a7073ac56544fd97514ac201
Dm-Degtiarev <9774385859@mail.ru>
2023-07-16 20:21:48 +0300

    настроено JWT, закрыт каждый эндпоинт авторизацией

--- online_training/views.py
+++ online_training/views.py
@@ -1,16 +1,20 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, generics
 from rest_framework.filters import OrderingFilter
+from rest_framework.permissions import IsAuthenticated
+
 from online_training.models import Course, Lesson, Payment
 from online_training.serializers import CourseSerializer, LessonSerializer, PaymentSerializer
 
 
 # ViewSets
 class CourseViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
 
 class PaymentViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     serializer_class = PaymentSerializer
     queryset = Payment.objects.all()
 
@@ -20,21 +24,26 @@ class PaymentViewSet(viewsets.ModelViewSet):
 
 # Generics
 class LessonListView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
 
 class LessonCreateView(generics.CreateAPIView):
+    permission_classes = [IsAuthenticated]
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
 
 class LessonDeleteView(generics.DestroyAPIView):
+    permission_classes = [IsAuthenticated]
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
 
 class LessonDetailView(generics.RetrieveAPIView):
+    permission_classes = [IsAuthenticated]
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
 
 class LessonUpdateView(generics.UpdateAPIView):
+    permission_classes = [IsAuthenticated]
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
